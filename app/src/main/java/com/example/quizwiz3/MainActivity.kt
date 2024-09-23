4f7ec17a4891b6f94588af2b154dac8b5bc03beb
simone-eve <st10215860@vcconnect.edu.za>
2024-09-23 23:20:20 +0200

    Completed Register SSO

--- app/src/main/java/com/example/quizwiz3/MainActivity.kt
+++ app/src/main/java/com/example/quizwiz3/MainActivity.kt
@@ -1,26 +1,24 @@
 package com.example.quizwiz3
 
-
 import android.R.attr.name
 import android.content.Intent
 import android.os.Bundle
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
-import api.DataModel
-import api.RetrofitAPI
 import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.gms.common.SignInButton
 import com.google.android.gms.common.api.ApiException
 import com.google.firebase.FirebaseApp
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-
 
 class MainActivity : AppCompatActivity() {
 
@@ -32,20 +30,24 @@ class MainActivity : AppCompatActivity() {
     private lateinit var btnSignUp: Button
     private lateinit var tvRedirectLogin: TextView
     private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         FirebaseApp.initializeApp(this)
 
-        gso =
-            GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build()
+        gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.google_client_id))
+            .requestEmail()
+            .build();
         gsc = GoogleSignIn.getClient(this, gso!!)
         val acct = GoogleSignIn.getLastSignedInAccount(this)
 //        if (acct != null) {
 //            navigateToSecondActivity()
 //        }
         val googleSignInButton = findViewById<SignInButton>(R.id.signIn)
-        googleSignInButton.setOnClickListener { signIn() }
+        googleSignInButton.setOnClickListener {signIn() }
+
 
         etEmail = findViewById(R.id.etEmailAddress)
         etPasword = findViewById(R.id.etPassword)
@@ -57,9 +59,8 @@ class MainActivity : AppCompatActivity() {
         auth = FirebaseAuth.getInstance()
 
         btnSignUp.setOnClickListener {
-             signUpUser()
+            signUpUser()
         }
-
 
         tvRedirectLogin.setOnClickListener {
             val intent = Intent(this, LoginActivity::class.java)
@@ -67,25 +68,22 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-
     private fun signUpUser() {
         val email = etEmail.text.toString()
         val password = etPasword.text.toString()
 
-        if (email.isBlank() || password.isBlank() ) {
+        if (email.isBlank() || password.isBlank()) {
             Toast.makeText(this, "Email and Password can't be blank", Toast.LENGTH_SHORT).show()
             return
         } else {
+            auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(this) {
+                if (it.isSuccessful) {
+                    Toast.makeText(this, "Successfully Signed Up", Toast.LENGTH_SHORT).show()
 
-                auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(this) {
-                    if (it.isSuccessful) {
-                        Toast.makeText(this, "Successfully Signed Up", Toast.LENGTH_SHORT).show()
-                       
-                    } else {
-                        Toast.makeText(this, "Sign Up Failed!", Toast.LENGTH_SHORT).show()
-                    }
+                } else {
+                    Toast.makeText(this, "Sign Up Failed!", Toast.LENGTH_SHORT).show()
                 }
-
+            }
         }
     }
 
@@ -99,64 +97,37 @@ class MainActivity : AppCompatActivity() {
         if (requestCode == 1000) {
             val task = GoogleSignIn.getSignedInAccountFromIntent(data)
             try {
-                task.getResult(ApiException::class.java)
-                navigateToSecondActivity()
-                Toast.makeText(applicationContext, "Signed In", Toast.LENGTH_SHORT)
-                    .show()
-
+               val account = task.getResult(ApiException::class.java)
+                Toast.makeText(applicationContext,
+                    "Signed in", Toast.LENGTH_SHORT).show()
+                firebaseAuthWithGoogle(account.idToken)
 
             } catch (e: ApiException) {
-                Toast.makeText(applicationContext, "Something went wrong", Toast.LENGTH_SHORT)
-                    .show()
+                Toast.makeText(applicationContext,
+                "Something went wrong", Toast.LENGTH_SHORT).show()
             }
         }
+    }
+    private fun firebaseAuthWithGoogle(idToken: String?) {
+        val credential = GoogleAuthProvider.getCredential(idToken, null)
+
+        auth.signInWithCredential(credential)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    navigateToSecondActivity()
+                    val user = auth.currentUser
+                    val displayName = user?.displayName ?: "User"
+                    Toast.makeText(this, "Signed In with Google $displayName" , Toast.LENGTH_SHORT).show()
+                } else {
+                    // Sign in failed
+                    Toast.makeText(this, "Firebase Authentication Failed", Toast.LENGTH_SHORT).show()
+                }
+            }
     }
 
     fun navigateToSecondActivity() {
         finish()
-        val intent: Intent = Intent(this@MainActivity, SecondActivity::class.java)
+        val intent: Intent = Intent(this@MainActivity, LoginActivity::class.java)
         startActivity(intent)
     }
-
-    private fun postData(name: String, email: String) {
-
-        val retrofit = Retrofit.Builder()
-            .baseUrl("https://localhost:7089/api/PostRegister")
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
-
-
-        // Creating an instance of the API interface
-//        val retrofitAPI = retrofit.create(RetrofitAPI::class.java)
-//
-//        // Passing data to the data model
-//        val model = DataModel(name, email)
-//
-//        // Creating a POST request
-//        val call: Call<DataModel> = retrofitAPI.createPost(model)
-//
-//        // Executing the request
-//        call.enqueue(object : Callback<DataModel> {
-//            override fun onResponse(call: Call<DataModel>, response: Response<DataModel>) {
-//                // Handle the response from the API
-//                Toast.makeText(this@MainActivity, "Data added to API", Toast.LENGTH_SHORT).show()
-////                nameEdt.text.clear()
-////                jobEdt.text.clear()
-//
-//                val responseFromAPI = response.body()
-//                val responseString = "Response Code : ${response.code()}\nName : ${responseFromAPI?.name}\nEmail : ${responseFromAPI?.email}\n"
-////                        "FirebaseUUID : ${responseFromAPI?.firebaseUUID}"
-//                Toast.makeText(applicationContext, "${responseString}", Toast.LENGTH_SHORT)
-//                    .show()
-//            }
-//
-//            override fun onFailure(call: Call<DataModel>, t: Throwable) {
-//                // Handle errors
-//                Toast.makeText(applicationContext, "Error found is: ${t.message}", Toast.LENGTH_SHORT)
-//                    .show()
-//            }
-//        })
-    }
-
-
 }
